44c5e0939ea38ce1e0b6b386503aa05ece133751
Giorgio <gorgo.durante@gmail.com>
2026-01-12 11:09:25 +0100

    Initial project setup

---
+++ StackBuildingProject/StackBuildingProject/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  StackBuildingProject
+//
+//  Created by Giorgio Durante on 12/01/26.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ StackBuildingProject/StackBuildingProject/StackBuildingProjectApp.swift
@@ -0,0 +1,33 @@
+//
+//  StackBuildingProjectApp.swift
+//  StackBuildingProject
+//
+//  Created by Giorgio Durante on 12/01/26.
+//
+
+import SwiftUI
+
+@main
+struct StackBuildingProjectApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
